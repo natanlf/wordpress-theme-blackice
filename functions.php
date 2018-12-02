c22bcd875be49a780019af1d132c33ef7b88a8e6
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-01 22:44:46 -0200

    Ajuste de css para os posts. Teste tema filho

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ require get_template_directory() . '/inc/customizer.php';
  	wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
 	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0', 'all' );
 	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
+	//wp_enqueue_style( 'template', get_stylesheet_uri());
 }
 	add_action('wp_enqueue_scripts', 'load_scripts'); 
 
---
+++ page-gallery.php
@@ -0,0 +1,88 @@
+<?php 
+/*
+Template Name: Gallery Home
+*/
+?>
+
+<?php get_header(); ?>
+	<div class="content-area">
+		<main>
+			<section class="slide">
+			<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+			</section>
+			<section class="services">
+				<div class="container">
+					<h2><?php _e( 'Our Services', 'wpwordpress' ); ?></h2>
+					<div class="row">
+							<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-1' )){
+									dynamic_sidebar( 'services-1' );
+								}
+ 								?>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-2' )){
+									dynamic_sidebar( 'services-2' );
+								}
+ 								?>
+							</div>
+						</div>
+ 						<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-3' )){
+									dynamic_sidebar( 'services-3' );
+								}
+ 								?>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</section>
+			<section class="middle-area">
+				<div class="container">
+					<div class="row">
+						<div class="news col-md-8">
+						<div class="container">
+							<!--<h2><?php _e( 'Latest News', 'wpwordpress' ); ?></h2>-->
+								<div class="row">
+									<?php 
+									// Segundo Loop
+	
+									$args = array(
+										'post_type' => 'post',
+										'posts_per_page' => $per_page,
+										'category__not_in' => $loop2_cat_exclude,
+										'category__in' => $loop2_cat_include,
+										'offset' => 0
+									);
+ 									$secondary = new WP_Query( $args );
+ 									if( $secondary->have_posts() ):
+										while( $secondary->have_posts() ): $secondary->the_post();
+
+									?>
+
+									<!-- Teremos 6 colunas para cada post, desde os dispostivos pequenos até os grandes -->
+									<div class="col-sm-12">
+										<?php get_template_part( 'template-parts/content', 'small' ); ?>
+									</div>
+ 									<?php
+										endwhile;
+										wp_reset_postdata();
+									endif;									
+									?>
+								</div>
+							</div>
+ 						</div>	
+ 						<?php get_sidebar( 'home' ); ?>					
+					</div>
+				</div>				
+			</section>
+		</main>
+	</div>
+<?php get_footer(); ?> 
