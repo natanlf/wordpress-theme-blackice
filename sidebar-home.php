a09109565d57e1044f45944289882342434c5486
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-05-18 23:04:56 -0300

    Novo template da pagina Home
    Agora os posts estão centralizados na tela porque tenho mas espaço de largura disponível.

--- sidebar-home.php
+++ sidebar-home.php
@@ -2,7 +2,7 @@
 Lá no functions php que é onde registro a sidebar, devemos colocar o id que colocamos lá aqui.
 Esse h-100 dentro da tag aside faz com que a barra lateral não se extenda até o fim da página -->
 <?php if( is_active_sidebar( 'sidebar-1' ) ): ?>
-	<aside class="sidebar col-md-4 h-100">
+	<aside class="sidebar col-md-3 col-sm-12 h-100 mr-md-3">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside>
 <?php endif; ?> 
